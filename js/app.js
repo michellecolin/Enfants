5fbf3b6dcb33b1e0087e49fec8a30fb2568b3021
michellecolin <michellecolin@users.noreply.github.com>
2017-07-15 00:06:17 -0300

    Add local storage and call to external api

--- js/app.js
+++ js/app.js
@@ -1,48 +1,55 @@
-angular.module("register", ['ui.mask']).controller("registerCtrl", function($scope, $timeout) {
+angular.module("register", ['ui.mask']).controller("registerCtrl", function($scope, $timeout, $http) {
 	$scope.showList = true;
 	$scope.newRegister = false;
 	$scope.alert = {show: false};
 	$scope.cadastro = "Novo Cadastro";
+	$scope.veicles = {};
+	$scope.editing = false;
 
-	$scope.registers = [
-		{
-			cpf: 1,
-			name: "Maria Dirceu",
-			birthDate: "1993-14-04",
-			address: "Rua dr. odilon galotti m: 433",
-			veicle: "aldi"
-		},
-		{
-			cpf: 1,
-			name: "Joana Dirceu",
-			birthDate: "1993-14-04",
-			address: "Rua dr. odilon galotti m: 433",
-			veicle: "aldi"
-		},
-		{
-			cpf: 1,
-			name: "José Silva",
-			birthDate: "1993-14-04",
-			address: "Rua dr. odilon galotti m: 433",
-			veicle: "aldi"
-		},
-		{
-			cpf: 1,
-			name: "Patricia Poeta",
-			birthDate: "1993-14-04",
-			address: "Rua dr. odilon galotti m: 433",
-			veicle: "aldi"
+	$http({
+	  method: 'GET',
+	  url: 'https://fipe-parallelum.rhcloud.com/api/v1/carros/marcas'
+		}).then(function successCallback(response) {
+			$scope.veicles.brands = response.data;
+  	}, function errorCallback(response) {
+  });
+
+	function convertDateObjects(registers) {
+		registers.forEach(function(register){
+			register.birthDate = new Date(register.birthDate);
+		});
+		return registers;
+  }
+
+	$scope.registers = [];
+	try{
+		var registers = JSON.parse(localStorage.getItem('registers'));
+		if (registers) {
+			registers = convertDateObjects(registers);
+			$scope.registers = registers;
 		}
-	];
+
+		console.log($scope.registers);
+	} catch(err) {
+		//err
+	}
 
 	$scope.addRegister = function(register) {
-		$scope.registers.push(register);
+		if (!$scope.editing) {
+			$scope.registers.push(register);
+			$scope.alert.message = "Novo cadastro adicionado!";
+		} else {
+			$scope.editing = false;
+			$scope.alert.message = "Cadastro editado!";
+			$scope.cadastro = "Novo Cadastro";
+		}
+		
 		delete $scope.register;
 		$scope.registerForm.$setPristine();
 		$scope.newRegister = false;
 		$scope.showList = true;
-		$scope.alert.message = "Novo cadastro adicionado!";
 		$scope.alert.show = true; 
+		saveOnLocalStorage();
 
 		$timeout(function() {
 			$scope.alert.show = false;
@@ -53,13 +60,18 @@ angular.module("register", ['ui.mask']).controller("registerCtrl", function($sco
 		$scope.cadastro = "Editar Cadastro";
 		$scope.showList = false;
 		$scope.newRegister = true;
+		$scope.editing = true;
+		$scope.veicles.models = [register.model];
 		$scope.register = register;
+		console.log(register);
+
 	};
 
 	$scope.removeRegister = function(index) {
 		$scope.registers.splice(index, 1);
 		$scope.alert.message = "Cadastro removido!";
 		$scope.alert.show = true; 
+		saveOnLocalStorage();
 
 		$timeout(function() {
 			$scope.alert.show = false;
@@ -70,6 +82,20 @@ angular.module("register", ['ui.mask']).controller("registerCtrl", function($sco
 		$scope.registers = registers.filter(function (register) {
 			if (!register.selected) return register;
 		});
+		saveOnLocalStorage();
 	};
 
+	$scope.showModels = function(brand) {
+		$http({
+		  method: 'GET',
+		  url: 'https://fipe-parallelum.rhcloud.com/api/v1/carros/marcas/' + brand + '/modelos'
+			}).then(function successCallback(response) {
+				$scope.veicles.models = response.data.modelos;
+	  	}, function errorCallback(response) {
+	  });
+	};
+
+	function saveOnLocalStorage() {
+		localStorage.setItem('registers', JSON.stringify($scope.registers));
+	}
 });
